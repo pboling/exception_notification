cc7f874c78d76418d51e287b33cd6e442c77d0f7
Lin Jen-Shin <godfat@godfat.org>
2009-06-15 16:40:42 +0800

    respect rails 2.3 !!!

--- lib/exception_notifiable.rb
+++ lib/exception_notifiable.rb
@@ -46,14 +46,6 @@ module ExceptionNotifiable
         write_inheritable_attribute(:exception_data, deliverer)
       end
     end
-
-    def exceptions_to_treat_as_404
-      exceptions = [ActiveRecord::RecordNotFound,
-                    ActionController::UnknownController,
-                    ActionController::UnknownAction]
-      exceptions << ActionController::RoutingError if ActionController.const_defined?(:RoutingError)
-      exceptions
-    end
   end
 
   private
@@ -63,37 +55,20 @@ module ExceptionNotifiable
       !self.class.local_addresses.detect { |addr| addr.include?(remote) }.nil?
     end
 
-    def render_404
-      respond_to do |type|
-        type.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found" }
-        type.all  { render :nothing => true, :status => "404 Not Found" }
-      end
-    end
-
-    def render_500
-      respond_to do |type|
-        type.html { render :file => "#{RAILS_ROOT}/public/500.html", :status => "500 Error" }
-        type.all  { render :nothing => true, :status => "500 Error" }
-      end
-    end
-
     def rescue_action_in_public(exception)
-      case exception
-        when *self.class.exceptions_to_treat_as_404
-          render_404
+      super
 
-        else          
-          render_500
+      return unless :internal_server_error ==
+        response_code_for_rescue(exception)
 
-          deliverer = self.class.exception_data
-          data = case deliverer
-            when nil then {}
-            when Symbol then send(deliverer)
-            when Proc then deliverer.call(self)
-          end
-
-          ExceptionNotifier.deliver_exception_notification(exception, self,
-            request, data)
+      deliverer = self.class.exception_data
+      data = case deliverer
+        when nil then {}
+        when Symbol then send(deliverer)
+        when Proc then deliverer.call(self)
       end
+
+      ExceptionNotifier.deliver_exception_notification(
+        exception, self, request, data)
     end
 end
