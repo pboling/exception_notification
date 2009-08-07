9098743b30177e855942d41775f648b39fd6da44
pboling <peter.boling@peterboling.com>
2009-08-06 20:17:05 -0400

    cleaning up init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,1 @@
-require "action_mailer"
-require "exception_notifier"
-require "exception_notifiable"
-require "exception_notifier_helper"
-
-#Object.class_eval do include Notifiable end
+require File.dirname(__FILE__) + "/rails/init"
